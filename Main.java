137050033018a11087d01fdad0be91e5d77faaeb
maxtauro <maxtauro@hotmail.com>
2016-12-29 22:43:06 -0500

    Release 1.1, set LatLngBounds from list of locations

--- Main.java
+++ Main.java
@@ -1,16 +1,64 @@
 package com.example.max.instamap;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Main extends FragmentActivity implements OnMapReadyCallback {
+
+public class Main extends FragmentActivity implements
+
+        OnMapReadyCallback, OnInfoWindowClickListener {
+    private LatLngBounds.Builder bounds;
+
+
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    private static class NamedLocation {
+
+        public final String name;
+        public final LatLng location;
+
+        NamedLocation(String name, LatLng location) {
+            this.name = name;
+            this.location = location;
+        }
+    }
+
+    private static final NamedLocation[] LIST_LOCATIONS = new NamedLocation[]{
+            new NamedLocation("Brooklyn Bridge", new LatLng(40.706086, -73.996864)),
+            new NamedLocation("Times Square", new LatLng(40.7583595, -73.9864889)),
+            new NamedLocation("Staten Island Ferry", new LatLng(40.6719458, -74.0424948)),
+            new NamedLocation("Koneko Cat Cafe", new LatLng(40.7204578, -73.9841388)),
+            new NamedLocation("Oculus WTC", new LatLng(40.71137299999999, -74.01227299999999)),
+            new NamedLocation("Driftwood Martial Arts", new LatLng(43.42177299999999, -80.55811799999999)),
+    };
+
+    private static final int[] ImageID = new int[]{
+            R.drawable.brooklyn_bridge,
+            R.drawable.times_square,
+            R.drawable.staten_ferry,
+            R.drawable.koneko,
+            R.drawable.oculus_wtc,
+            0,
+    };
+
 
     private GoogleMap mMap;
 
@@ -22,6 +70,7 @@ public class Main extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -38,33 +87,59 @@ public class Main extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         LatLng manhattan = new LatLng(40.783060, -73.971249);
-        for (int i=0; i<LIST_LOCATIONS.length; i++){
-            mMap.addMarker(new MarkerOptions().position(LIST_LOCATIONS[i].location)
+        addMarkersToMap();
+
+        // Setting an info window adapter allows us to change the both the contents and look of the
+        // info window.
+        mMap.setOnInfoWindowClickListener(this);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(manhattan, 10f));
+
+        /*// Set listeners for marker events.  See the bottom of this class for their behavior.
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnInfoWindowClickListener(this);
+        mMap.setOnMarkerDragListener(this);
+        mMap.setOnInfoWindowCloseListener(this);
+        mMap.setOnInfoWindowLongClickListener(this);*/
+
+        final View mapView = getSupportFragmentManager().findFragmentById(R.id.map).getView();
+        if (mapView.getViewTreeObserver().isAlive()) {
+            mapView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @SuppressWarnings("deprecation") // We use the new method when supported
+                @SuppressLint("NewApi") // We check which build version we are using.
+                @Override
+                public void onGlobalLayout() {
+                    /*LatLngBounds bounds = new LatLngBounds.Builder()
+                            .include(LIST_LOCATIONS[0].location)
+                            .include(LIST_LOCATIONS[1].location)
+                            .include(LIST_LOCATIONS[2].location)
+                            .include(LIST_LOCATIONS[3].location)
+                            .include(LIST_LOCATIONS[4].location)
+                            .include(LIST_LOCATIONS[5].location)
+                            .build();*/
+                    bounds = new LatLngBounds.Builder();
+                    for (int i = 0; i < LIST_LOCATIONS.length; i++) {
+                        bounds.include(LIST_LOCATIONS[i].location);
+                    }
+
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                        mapView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    } else {
+                        mapView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 50));
+                }
+            });
+        }
+    }
+
+    private void addMarkersToMap() {
+        for (int i = 0; i < LIST_LOCATIONS.length; i++) {
+            mMap.addMarker(new MarkerOptions()
+                    .position(LIST_LOCATIONS[i].location)
                     .title((LIST_LOCATIONS[i].name))
-                    );
-        }
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(manhattan));
-
-       // mMap.addMarker(new MarkerOptions().position(manhattan).title("Marker in Sydney"));
-    }
-
-
-    private static class NamedLocation {
-
-        public final String name;
-        public final LatLng location;
-
-        NamedLocation(String name, LatLng location) {
-            this.name = name;
-            this.location = location;
+            );
         }
     }
-    private static final NamedLocation[] LIST_LOCATIONS = new NamedLocation[]{
-            new NamedLocation("Brooklyn Bridge", new LatLng(40.706086, -73.996864)),
-            new NamedLocation("Times Square", new LatLng(40.7583595, -73.9864889)),
-            new NamedLocation("Staten Island Ferry", new LatLng(40.6719458, -74.0424948)),
-            new NamedLocation("Koneko Cat Cafe", new LatLng(40.7204578, -73.9841388)),
-            new NamedLocation("Occulus WTC", new LatLng(40.71137299999999, -74.01227299999999)),
-            new NamedLocation("Driftwood Martial Arts", new LatLng(43.42177299999999, -80.55811799999999)),
-    };
+
+
 }
